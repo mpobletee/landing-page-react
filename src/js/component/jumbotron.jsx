70d1dd01d18d0429f78d77a3f2e3d4c2cc277473
Victor Tapia Magna <vhtapiamagna@gmail.com>
2022-12-19 19:32:18 +0000

    revision

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 export const Jumbotron = (props) => {
     return (
-        <div className="jumbotron m-5  text-center bg-light" id="jumbito">
+        <div className="jumbotron m-5  text-center bg-light" id="jumbo">
             <h1 className="display-4">{props.title}</h1>
             <p className="lead">{props.description}</p>
             <a className="btn btn-primary btn-lg" href={props.buttonURL} role="button" target="_blank">{props.buttonLabel}</a>
